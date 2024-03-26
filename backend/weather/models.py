5a676ff435e8f6acc7516a1e7be9ce5983e9278d
MarkStanly <nhien.nguyenroad2mag80@hcmut.edu.vn>
2024-03-26 23:11:09 +0700

    celery cannot identify task

--- backend/weather/models.py
+++ backend/weather/models.py
@@ -1,10 +1,37 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 class Entry(models.Model):
-    name = models.CharField(max_length=100)
-    value = models.IntegerField()
+    time = models.DateField(null=True)
+    temp = models.DecimalField(max_digits=6, decimal_places=1, default=-1)
+    dwpt = models.DecimalField(max_digits=6, decimal_places=1, default=-1)
+    rhum = models.DecimalField(max_digits=6, decimal_places=1, default=-1)
+    prcp = models.DecimalField(max_digits=6, decimal_places=1, default=-1)
+    wdir = models.DecimalField(max_digits=6, decimal_places=1, default=-1)
+    wspd = models.DecimalField(max_digits=6, decimal_places=1, default=-1)
+    pres = models.DecimalField(max_digits=6, decimal_places=1, default=-1)
+    coco = models.IntegerField(
+        validators=[
+            MinValueValidator(0),  # Lower limit
+            MaxValueValidator(27),  # Upper limit
+        ],
+        default = 0
+    )
 
+    # series' time value is "name"
+    def add_entry(s):
+        e = Entry(time=s.name,
+                  temp=s.temp,
+                  dwpt=s.dwpt,
+                  rhum=s.rhum,
+                  prcp=s.prcp,
+                  wdir=s.wdir,
+                  wspd=s.wspd,
+                  pres=s.pres,
+                  coco=s.coco)
+        e.save()
+        return 
+    
     def __str__(self):
-            """String representation of the model."""
-            return str(self.id) + ":" + self.name
+        return str(self.time) + " : " + self.temp + " Cel"
--- backend/weather/tasks.py
+++ backend/weather/tasks.py
@@ -1,14 +1,32 @@
+# Task scheduler
 from celery import shared_task
+
+# Other libraries
+from datetime import datetime
+from meteostat import Hourly
+
+# Importing entry model
+from .models import *
 
 # Define your tasks here
 @shared_task
-def simple():
-    print("Running properly")
+def ping():
+    print("Pong! Your async completed!")
     return
 
-from .models import *
 @shared_task
-def create_new():
-    e = Entry.objects.create(name="new thang",value=1)
-    e.save()
+def update_entry():
+    # Set time period
+    start = datetime(2024, 1, 1)
+    end = datetime(2024, 12, 31, 23, 59)
+    
+    # Get hourly data
+    data = Hourly('72219', start, end)
+    data = data.fetch()
+    
+    # Saving hourly data as entries
+    for i in range(len(data)):
+        # Access row data using df.iloc[i]
+        Entry.add_entry(data.iloc[i])
+        
     return
