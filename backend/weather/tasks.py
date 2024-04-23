2b7fd1aac85907f458c3924ca163e805118bbc4a
MarkStanly <nhien.nguyenroad2mag80@hcmut.edu.vn>
2024-04-23 23:32:48 +0700

    finalizing

--- backend/weather/tasks.py
+++ backend/weather/tasks.py
@@ -38,7 +38,7 @@ parameter_dict_test =  {'temp': [(1,0,1),(1,0,1,8), 'c', 'cg'],
                         'pres': [(1,0,1),(1,0,1,8), 'c', 'cg']}
 
 # SARIMA components deduced from entry lag
-parameter_dict = {'temp': [(2,1,2),(2,0,3,8), 'c', 'lbfgs', 50],                                       # Need testing 3 -> 6, overall pretty good
+parameter_dict = {'temp': [(2,1,2),(2,0,3,8), 'c', 'lbfgs', 50],                                       # "Synced"
                   'dwpt': [([1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1],0,2),(0,0,2,24), 'ct', 'cg', 50], # Literally unsynced
                   'rhum': [(22,1,0),(0,0,6,8), 'c', 'cg', 50],                                         # Need testing
                   'wspd': [([1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1],0,2),(0,0,2,24), 'c', 'cg', 50],  # Synced
@@ -82,13 +82,13 @@ rain_or_not = {1: 0,
 
 # Define your tasks here
 # Task for async testing
-@shared_task
+@shared_task(acks_late=False, reject_on_worker_lost=True)
 def ping():
     print("Pong! Your async completed!")
     return
 
 # Get weather data on startup and after a time period
-@shared_task
+@shared_task(acks_late=False, reject_on_worker_lost=True)
 def update_entry():
     # Get latest entry time from database
     latest = Entry.objects.latest('time') if len(Entry.objects.all()) != 0 else datetime(2017, 1 ,1)
@@ -100,6 +100,9 @@ def update_entry():
     # Get hourly data (from a specific weather station: 48900)
     data = Hourly('48900', start, end)
     data = data.fetch()
+    
+    # Calculating accuracy
+    accuracy.delay()
     
     if not data.empty:
         # Filling nan values using previous values
@@ -114,63 +117,60 @@ def update_entry():
         arima.delay() 
     else:
         print ("data is up to date")
-    
-    # Calculating accuracy
-    accuracy.delay()
     return
 
 # Processing weather data 
-@shared_task
+@shared_task(acks_late=False, reject_on_worker_lost=True)
 def arima():
     train_set = (read_frame(Entry.objects.all())[['time','temp','dwpt','rhum','wdir','wspd','pres','coco']]).copy(deep=True)
         
-    # Preparing predictions set
-    predictions = (read_frame(Entry.objects.all().order_by('-id')[:entry_cycle])[['time','temp','dwpt','rhum','wdir','wspd','pres','coco']]).copy(deep=True)
-    predictions = predictions.sort_values(by='time')
-    predictions.reset_index(drop=True, inplace=True)
+    # # Preparing predictions set
+    # predictions = (read_frame(Entry.objects.all().order_by('-id')[:entry_cycle])[['time','temp','dwpt','rhum','wdir','wspd','pres','coco']]).copy(deep=True)
+    # predictions = predictions.sort_values(by='time')
+    # predictions.reset_index(drop=True, inplace=True)
     
-    # Converting from decimal to numpy float64
-    train_set[dimensions] = train_set[dimensions].astype(np.float64)
-    predictions[dimensions] = predictions[dimensions].astype(np.float64)
+    # # Converting from decimal to numpy float64
+    # train_set[dimensions] = train_set[dimensions].astype(np.float64)
+    # predictions[dimensions] = predictions[dimensions].astype(np.float64)
     
-    # Specific customizations (1)
-    train_set['pres'] = train_set['pres'] - 1000
+    # # Specific customizations (1)
+    # train_set['pres'] = train_set['pres'] - 1000
     
-    for index in range(0,len(predictions)):
-        predictions.loc[index, 'time'] += dt.timedelta(hours=entry_cycle)
+    # for index in range(0,len(predictions)):
+    #     predictions.loc[index, 'time'] += dt.timedelta(hours=entry_cycle)
         
-    # Conduct SARIMA on every dimension/feature
-    for dimension in tqdm(dimensions, desc="Predicting the next day's features..."):
-        # Init SARIMAX model
-        model = SARIMAX(np.asarray(train_set[dimension]), order=parameter_dict[dimension][0], seasonal_order=parameter_dict[dimension][1], trend=parameter_dict[dimension][2])
-        model_fit = model.fit(method=parameter_dict[dimension][3], maxiter=parameter_dict[dimension][4])
+    # # Conduct SARIMA on every dimension/feature
+    # for dimension in tqdm(dimensions, desc="Predicting the next day's features..."):
+    #     # Init SARIMAX model
+    #     model = SARIMAX(np.asarray(train_set[dimension]), order=parameter_dict[dimension][0], seasonal_order=parameter_dict[dimension][1], trend=parameter_dict[dimension][2])
+    #     model_fit = model.fit(method=parameter_dict[dimension][3], maxiter=parameter_dict[dimension][4])
 
-        # Making predictions
-        prediction = model_fit.forecast(entry_cycle)
-        predictions[dimension] = prediction
+    #     # Making predictions
+    #     prediction = model_fit.forecast(entry_cycle)
+    #     predictions[dimension] = prediction
         
-        # Deallocate memory
-        del model
-        del model_fit
-        gc.collect()
+    #     # Deallocate memory
+    #     del model
+    #     del model_fit
+    #     gc.collect()
 
-    # Specific customizations (2)
-    predictions['pres'] = predictions['pres'] + 1000
+    # # Specific customizations (2)
+    # predictions['pres'] = predictions['pres'] + 1000
         
-    # Create + save prediction instance
-    current_time = train_set.iloc[-1].time
-    new_prediction_ins = Prediction(prediction_time=current_time, predicted_entry_count=entry_cycle)
+    # # Create + save prediction instance
+    # current_time = train_set.iloc[-1].time
+    # new_prediction_ins = Prediction(prediction_time=current_time, predicted_entry_count=entry_cycle)
     
-    # Predict weather condition
-    predictions = coco(predictions)
-    predictions = rain(predictions)
+    # # Predict weather condition
+    # predictions = coco(predictions)
+    # predictions = rain(predictions)
     
-    # Create + save predicted entries
-    new_prediction_ins.save()
-    for i in tqdm(range(len(predictions)), desc="Saving predicted values..."):
-            Predicted_Entry.add_entry(predictions.iloc[i], new_prediction_ins)
+    # # Create + save predicted entries
+    # new_prediction_ins.save()
+    # for i in tqdm(range(len(predictions)), desc="Saving predicted values..."):
+    #         Predicted_Entry.add_entry(predictions.iloc[i], new_prediction_ins)
 
-@shared_task
+@shared_task(acks_late=False, reject_on_worker_lost=True)
 def coco(predictions):
     # Preparing training set
     # Chooose a start date
@@ -233,7 +233,7 @@ def coco(predictions):
     return predictions
 
 # Predict risk of raining
-@shared_task
+@shared_task(acks_late=False, reject_on_worker_lost=True)
 def rain(predictions):
     # Preparing training set
     # Chooose a start date
@@ -280,7 +280,7 @@ def rain(predictions):
     return predictions
 
 # Calculating accuracy
-@shared_task
+@shared_task(acks_late=False, reject_on_worker_lost=True)
 def accuracy():
     # Getting predictions that have been predicted over 24 hours ago
     est_elapsed = (dt.datetime.utcnow() - dt.timedelta(hours=entry_cycle)).replace(tzinfo=pytz.utc)
